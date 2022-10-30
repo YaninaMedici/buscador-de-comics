fb53e20fd8791babed4ca875303969fdc2b64104
YaniMedici <mediciyanina@gmail.com>
2022-10-30 14:53:52 -0300

    Corrección del código de main.js con comics y personajes en el orden correcto

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+//**********************************************/
+// COMICS
+//**********************************************/
+
+
 // siempre los parametros tienen que ir en la url
 
 
@@ -57,6 +62,73 @@ const loadComics = async () => {
 loadComics();
 
 
-// comics
-//     .then(response => response.json())
-//     .then(data => console.log(data))
+//**********************************************/
+// PERSONAJES
+//**********************************************/
+//Belu personajes
+
+const loadCharacters = async () => {
+    const charactersResponse = await getCharacters()
+    const data = charactersResponse.data
+    const characters = data.results
+
+    const charactersResults = document.getElementById('characters-results');
+    const container = document.createElement('div');
+    const row = document.createElement('div')
+
+    charactersResults.appendChild(container);
+    container.appendChild(row);
+
+    container.classList.add('container');
+    // ***********************
+    // OCULTE EL CONTAINER PARA QUE NO MOLESTE, SI SACA LA CLASE SE VEN LOS PERSONAJES. 
+    container.classList.add('visually-hidden');
+
+    row.classList.add('row');
+
+    for (const character of characters) {
+        const name = document.createElement('h2');
+        const nameText = document.createTextNode(character.name);
+
+        card.appendChild(cardImg);
+        card.appendChild(cardBody);
+        col.appendChild(card)
+        cardBody.appendChild(name);
+        name.appendChild(nameText)
+
+        col.classList.add('col-md-3');
+        title.classList.add('h6'); // va a tener una importancia de un h2, pero boostrap permite que se vea como un h5
+    };
+};
+
+loadCharacters()
+
+
+
+//**********************************************/
+// Form filtros
+//**********************************************/
+const controlorderBy = document.getElementById('control-order-by');
+const searchType = document.getElementById('search-type');
+
+
+const generarOption = ()=> {
+    if(searchType.value === 'comics'){
+        controlorderBy.innerHTML = `
+      <option value="title">A-Z</option>
+      <option value="-title">Z-A</option>
+      <option value="-onsaleDate">Más nuevos</option>
+      <option value="onsaleDate">Más viejos</option> `
+    }
+    if(searchType.value === 'characters'){
+        controlorderBy.innerHTML = `
+      <option value="name">A-Z</option>
+      <option value="-name">Z-A</option> `
+    }
+};
+
+generarOption()
+
+// searchType.addEventListener('change', () => {
+//     generarOption()
+// };
