3c2900680116f4092f0f11fff2f9e5946d87ff99
Belen Sanchez <102567483+Belu-Sanchez@users.noreply.github.com>
2022-10-31 14:01:51 -0300

    update cambio url

--- api/characters.js
+++ api/characters.js
@@ -1,7 +1,7 @@
 const getCharacters = async (offset, orderBy) =>{     
  
    
-    const response = await fetch(`${baseUrl}characters?&apikey=${apikey}&offset=${offset}&orderBy=${orderBy}`);
+    const response = await fetch(`${baseUrl}/characters?&apikey=${apikey}&offset=${offset}&orderBy=${orderBy}`);
     const data = await response.json();
     return data
     
--- api/comics.js
+++ api/comics.js
@@ -3,7 +3,7 @@ const apikey = `ad0c07cf382bdeb7bad248929fa3e0d6`;
 const apiPrivate = '647db36cd1ad69c1263e719c49cc462cd0bd8519';
 
 const getComics = async (offset, orderBy) => {
-    const response = await fetch(`${baseUrl}comics?&apikey=${apikey}&offset=${offset}&orderBy=${orderBy}`)
+    const response = await fetch(`${baseUrl}/comics?&apikey=${apikey}&offset=${offset}&orderBy=${orderBy}`)
 
     const data = await response.json()
     return data;
