63f2e883455286218faa0fe9b4fbf185ca01def8
Belen Sanchez <102567483+Belu-Sanchez@users.noreply.github.com>
2022-10-31 14:58:10 -0300

    update/viendo si carga la data en el deployado

--- api/comics.js
+++ api/comics.js
@@ -1,27 +1,8 @@
-const baseUrl = `https://gateway.marvel.com/v1/public`;
-const apikey = "ad0c07cf382bdeb7bad248929fa3e0d6";
-const apiPrivate = '647db36cd1ad69c1263e719c49cc462cd0bd8519';
+
 
 const getComics = async (offset, orderBy) => {
-    const response = await fetch(`${baseUrl}/comics?orderBy=${orderBy}&offset=${offset}&apikey=${apikey}`)
+    const response = await fetch(`${baseUrl}/comics?apikey=${apiKey}&offset=${offset}&orderBy=${orderBy}`)
     const data = await response.json()
     return data;
 }
 
-            //https://gateway.marvel.com/v1/public/comics?orderBy=title&offset=20&apikey=
-
-// let url 
-// if(searchType.value === 'comics'){
-//     if(searchInput == ""){
-//         url = `${baseUrl}/comics?&offset=${offset}&orderBy=${orderBy}&apikey=${apikey}`
-
-// }
-//     }else{
-//     url = `${baseUrl}/comics?&titleStartsWith=${searchInput}&offset=${offset}&orderBy=${orderBy}&apikey=${apikey}`
-
-// }
-
-//https://gateway.marvel.com/v1/public//comics?&apikey=26a7a8acd5fe326ac4725819538ebf98&offset=0&orderBy=title
-//https://gateway.marvel.com/v1/public/comics?apikey=ad0c07cf382bdeb7bad248929fa3e0d6&offset=0&orderBy=title
-//https://gateway.marvel.com/v1/public/comics?apikey=ad0c07cf382bdeb7bad248929fa3e0d6&offset=20&orderBy=title
-//https://gateway.marvel.com/v1/public/comics?orderBy=title&offset=20&apikey=
