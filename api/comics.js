d99de0df0d3d8b93aa76c83d7396e04f292034ab
Belen Sanchez <102567483+Belu-Sanchez@users.noreply.github.com>
2022-10-31 13:18:27 -0300

    update/cambios url por problemas en el deployado

--- api/comics.js
+++ api/comics.js
@@ -3,7 +3,7 @@ const apikey = "ad0c07cf382bdeb7bad248929fa3e0d6";
 const apiPrivate = '647db36cd1ad69c1263e719c49cc462cd0bd8519';
 
 const getComics = async (offset, orderBy) => {
-    const response = await fetch(`${baseUrl}/comics?&apikey=${apikey}&offset=${offset}&orderBy=${orderBy}`)
+    const response = await fetch(`${baseUrl}/comics?&offset=${offset}&orderBy=${orderBy}&apikey=${apikey}`)
 
     const data = await response.json()
     return data;
@@ -20,3 +20,4 @@ const getComics = async (offset, orderBy) => {
 //     url = `${baseUrl}/comics?&titleStartsWith=${searchInput}&offset=${offset}&orderBy=${orderBy}&apikey=${apikey}`
 
 // }
+
