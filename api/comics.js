cc127358adb64a368862fcaf51eed2d22e4c9853
Belen Sanchez <102567483+Belu-Sanchez@users.noreply.github.com>
2022-10-31 13:47:50 -0300

    update/cambio de claves de la api

--- api/comics.js
+++ api/comics.js
@@ -1,6 +1,6 @@
-const baseUrl = `https://gateway.marvel.com/v1/public/`;
-const apikey = 'ad0c07cf382bdeb7bad248929fa3e0d6';
-const apiPrivate = '647db36cd1ad69c1263e719c49cc462cd0bd8519';
+const apikey = '26a7a8acd5fe326ac4725819538ebf98';
+const apiPrivate = '36e9d123cce0e5998a27a9394da7867691023eb9';
+const baseUrl = `https://gateway.marvel.com/v1/public/`
 
 const getComics = async (offset, orderBy) => {
     const response = await fetch(`${baseUrl}/comics?&apikey=${apikey}&offset=${offset}&orderBy=${orderBy}`)
