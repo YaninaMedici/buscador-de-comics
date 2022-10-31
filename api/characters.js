d2841b96d0e7f0a089fbfedf64f43ed468a78487
Belen Sanchez <102567483+Belu-Sanchez@users.noreply.github.com>
2022-10-31 14:42:35 -0300

    update/cambios de url

--- api/characters.js
+++ api/characters.js
@@ -1,7 +1,7 @@
 const getCharacters = async (offset, orderBy) =>{     
  
    
-    const response = await fetch(`${baseUrl}/characters?apikey=${apikey}&offset=${offset}&orderBy=${orderBy}`);
+    const response = await fetch(`${baseUrl}/characters?orderBy=${orderBy}&offset=${offset}&apikey=${apikey}`);
     const data = await response.json();
     return data
     
--- api/comics.js
+++ api/comics.js
@@ -3,12 +3,12 @@ const apikey = "ad0c07cf382bdeb7bad248929fa3e0d6";
 const apiPrivate = '647db36cd1ad69c1263e719c49cc462cd0bd8519';
 
 const getComics = async (offset, orderBy) => {
-    const response = await fetch(`${baseUrl}/comics?apikey=${apikey}&offset=${offset}&orderBy=${orderBy}`)
-
+    const response = await fetch(`${baseUrl}/comics?orderBy=${orderBy}&offset=${offset}&apikey=${apikey}`)
     const data = await response.json()
     return data;
 }
 
+            //https://gateway.marvel.com/v1/public/comics?orderBy=title&offset=20&apikey=
 
 // let url 
 // if(searchType.value === 'comics'){
@@ -23,3 +23,5 @@ const getComics = async (offset, orderBy) => {
 
 //https://gateway.marvel.com/v1/public//comics?&apikey=26a7a8acd5fe326ac4725819538ebf98&offset=0&orderBy=title
 //https://gateway.marvel.com/v1/public/comics?apikey=ad0c07cf382bdeb7bad248929fa3e0d6&offset=0&orderBy=title
+//https://gateway.marvel.com/v1/public/comics?apikey=ad0c07cf382bdeb7bad248929fa3e0d6&offset=20&orderBy=title
+//https://gateway.marvel.com/v1/public/comics?orderBy=title&offset=20&apikey=
