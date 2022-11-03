55865bca02a8ddac412ccc35198cebb55df45b86
Belen Sanchez <102567483+Belu-Sanchez@users.noreply.github.com>
2022-11-03 19:10:39 -0300

    update/cambios en la url

--- api/comics.js
+++ api/comics.js
@@ -1,3 +1,7 @@
+const baseUrl = `https://gateway.marvel.com/v1/public`;
+const apiKey = 'ad0c07cf382bdeb7bad248929fa3e0d6';
+const apiPrivate = '647db36cd1ad69c1263e719c49cc462cd0bd8519';
+
 
 
 const getComics = async (offset, orderBy) => {
@@ -5,4 +9,3 @@ const getComics = async (offset, orderBy) => {
     const data = await response.json()
     return data;
 }
-
